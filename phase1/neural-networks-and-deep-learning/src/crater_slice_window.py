25f31286881829e2f742d598bba14b6b5db6d97d
Cristian <cristian@cs.umb.edu>
2018-05-15 00:00:57 -0400

    testing

--- phase1/neural-networks-and-deep-learning/src/crater_slice_window.py
+++ phase1/neural-networks-and-deep-learning/src/crater_slice_window.py
@@ -7,7 +7,7 @@ from helpers import *
 
 image_1 = cv2.imread("../../crater_dataset/crater_data/images/tile3_24.pgm")
 image_2 = cv2.imread("../../crater_dataset/crater_data/images/tile3_25.pgm")
-(winW, winH) = (50, 50)
+(winW, winH) = (100, 100)
 window_size = (winH, winW)
 
 images = [image_1] #, image_2]
@@ -23,7 +23,11 @@ def crater_sliding_window(image, stepSize):
 
 model = keras_cnn.network()
 og_image = image_1.copy()
-for (x, y, window, image_layer, layer) in crater_sliding_window(image_1, 25):
+prev_layer = 0
+for (x, y, window, image_layer, layer) in crater_sliding_window(image_1, 100):
+    if prev_layer != layer:
+        print "Working on layer %d" % (layer)
+        prev_layer = layer
     if np.argmax(model.predict(keras_cnn.cvImg2input(window))) == 1:
         #cv2.rectangle(image_layer,  (x, y), (x + winW, y + winH), (0, 255, 0), 2)
         cv2.rectangle(image_layer,  (x * layer * layer, y * layer * layer), 
